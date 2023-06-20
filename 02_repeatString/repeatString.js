3211cb2c35772c598f4e6d6f9a7b9973ec076e4a
izng-code <129785832+izng-code@users.noreply.github.com>
2023-06-20 12:08:09 +0200

    Do Exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concString = "";
+    if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            concString += string;
+        }
+        return concString;
+    }
 };
 
 // Do not edit below this line
