f86561676e59ebd5d24e8ab675b7084b2f493fd5
izng-code <129785832+izng-code@users.noreply.github.com>
2023-06-21 12:16:26 +0200

    Do exercises 3 to 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    const reverseArray = [];
+    for (let i = 0; i < array.length; i++) {
+        reverseArray.unshift(array[i]);
+    }
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, args) {
+    for (let i = 1; i < arguments.length; i++) {
+        if (array.includes(arguments[i])) {
+            array.splice(array.indexOf(arguments[i]), 1);
+        }
+        else {
+            continue;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
